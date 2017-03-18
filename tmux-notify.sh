8edbbab631eed69e9beaeec1463369f356903798
Mr Seth Price <ssterling@firemail.cc>
2017-03-18 19:28:56 -0400

    Initial commit

---
+++ tmux-notify.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+# 'tmux-notify' - simple tmux notification script
+# Copyright 2017 Seth Price
+# Released under the MIT License; see ./LICENSE for details
+
+message_store=${HOME}/.tmux-notify
+
+if [ ! -f $message_store ]; then
+    touch $message_store
+fi
+
+function tmux_notify {
+    #notification="[$(date)] $1: $2"
+    notification="$1: $2"
+    echo $notification >> $message_store
+
+    tmux_window=$(/usr/bin/tmux list-windows \
+        -F "#{window_active} #{window_index} #{window-name}" | \
+        sort | tail -n 1 | cut -d ' ' -f 2-)
+
+    # TODO: reset this to initial value later?
+    tmux set-option -t ${tmux_window} display-time 4000
+    tmux display-message "${notification}"
+    return $?
+}
+
+function tmux_notify_count {
+    if [ -e $message_store ]; then
+        message_count=$(wc -l ${message_store} | awk '{ print $1 }')
+    else
+        message_count=0
+    fi
+    echo $message_count
+}
+
+function tmux_notify_clear {
+    if [ -e $message_store ]; then
+        rm $message_store && touch $message_store
+    else
+        touch $message_store
+    fi
+    return $?
+}
+
+if [ "$1" == "count" ]; then
+    echo $(tmux_notify_count)
+elif [ "$1" == "clear" ]; then
+    tmux_notify_clear
+elif [ "$1" == "show" ]; then
+    cat $message_store
+    tmux_notify_clear
+elif [ "$1" == "show-nc" ]; then
+    cat $message_store
+else
+    tmux_notify $*
+fi
